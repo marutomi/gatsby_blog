fa1d4c948c2a8a93d2e254ea3953ce14ac148b4f
marutomi <flat.maru@gmail.com>
2021-09-26 21:23:29 +0900

    20210926 公開前にコミット

---
+++ content/shitagaki/20210925/index.md
@@ -0,0 +1,67 @@
+---
+title: nginxに触ってみよう
+date: "2021-09-25T16:12:37.121Z"
+---
+
+### dockerって何
+？触ってみよう！
+以下のさくらのナレッジに実際の手順が記載されていますので、これを見ながら実際に動かしてみるとよく分かります。
+私は一回動かしただけでは覚えきれず理解できなかったので、「centOSにnginxをインストールして手順をDokerFileにする」とかしてコマンド類を覚えました。
+[さくらのナレッジDoker入門](https://knowledge.sakura.ad.jp/13265/)
+
+## 作ってみたdocker ファイル
+まずcentOS-7にnginxをインストールする場合、yumリポジトリからインストールするために設定ファイルを追加する必要があります。
+以下サイトを参考にさせて頂きました。
+[Nginx を CentOS 7 にインストールする手順](https://weblabo.oscasierra.net/nginx-centos7-install/)
+事前にテキストエディタで以下のnginx.repoを作成しておきます。DockerFileと同じ階層に以下ファイルを置いておきます。
+```markdown
+[nginx]
+name=nginx repo
+baseurl=http://nginx.org/packages/centos/7/$basearch/
+gpgcheck=0
+enabled=1
+```
+そのファイルをdockerコンテナ内の
+/etc/yum.repos.d/nginx.repo
+として追加してあげる必要があるので、DockerFile内にその記述を記載します。
+
+centos内に配置するのではなく、コンテナを個別に分けた方が良いのでは？
+```markdown
+
+```
+
+
+### dockerコマンド
+```markdown
+ 411  docker run -it -d -p 18080:8080 -v --name nginx centos:7
+  412  docker ps -a
+  413  pwd
+  414  docker stop 83632206e391
+  415  docker rm 83632206e391
+  416  cd
+  417  ll
+  418  cd docker/
+  419  ll
+  420  docker run --help
+  421  docker images
+  422  docker run -it -d -p 18080:8080 -v --name centos:7
+  423  docker ps -a
+  424  docker stop a71a84136b63
+  425  docker run -it -d -p 18090:80  --name  testContainer2 centos:7
+  426  docker ps -a
+  427  docker rm a71a84136b63
+  428  history
+  docker$ docker exec -it testContainer2 bash
+
+  docker run -it -d -p 8181:80  -v /home/ubuntu/docker/nginx/log:/var/log/nginx/ --name nginx_share nginx
+
+```
+
+docker内でsystemctlは使用できない
+直接nginxやapacheの起動コマンドをたたいて起動するのが一般的
+/usr/sbin/nginx
+
+[参考：nginxの起動方法](https://www.server-memo.net/server-setting/nginx/nginx-command.html)
+
+
+
---
+++ gatsby-config.js
@@ -0,0 +1,77 @@
+module.exports = {
+  siteMetadata: {
+    title: `立位体前屈`,
+    author: {
+      name: `nappa`,
+      summary: `SEなんですが、グラフィック系とか3DCG、後は仕事の進め方とか色々書く人です。`,
+    },
+    description: `備忘録ブログ Gatsbyで更新してます`,
+    siteUrl: `https://kiwigumi.com/`,
+    social: {
+      twitter: `nappa84149358`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gatsby Starter Blog`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
